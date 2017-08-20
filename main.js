55554e18fdf4cff3c946877d87e30e98a7883495
Esther Leytush <eleytush@gmail.com>
2017-08-19 23:01:28 -0400

    add Bulma styles

--- main.js
+++ main.js
@@ -8,7 +8,9 @@ function attachEncryptForm(){
     if (stringToEncrypt.length && password.length) {
       var encryptedResult = CryptoJS.AES.encrypt(stringToEncrypt, password);
       var encryptedString = encryptedResult.toString();
+
       $("#encrypted-result").text(encryptedString);
+      $(".encryption-result-section").addClass("is-primary");
     }
   });
 }
@@ -23,15 +25,23 @@ function attachDecryptForm(){
     if (stringToDecrypt.length && password.length) {
       var decryptedResult = CryptoJS.AES.decrypt(stringToDecrypt, password);
       var decryptedString = decryptedResult.toString(CryptoJS.enc.Utf8);
+
       $("#decrypted-result").text(decryptedString);
+      $(".decryption-result-section").addClass("is-info");
     }
   });
 }
 
 function attachClearForm(){
-  $("#clear-form").on("click", function(e){
+  $(".clear-form").on("click", function(e){
     e.stopImmediatePropagation();
-    $(this).parent().find(".input-string").val("");
+
+    var form = $(this).parent().parent().parent();
+    $(form).find(".input-string").val("");
+    $(form).parent().find(".result-section").text("");
+
+    $(form).parent().find(".notification").removeClass("is-primary");
+    $(form).parent().find(".notification").removeClass("is-info")
   });
 }
 
